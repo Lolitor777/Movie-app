e94f1a9ac3c91a8a15b293225db0613a4cd2f889
Victor <carvajal57685@gmail.com>
2024-10-26 12:08:15 -0500

    Inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './movies/pages/home-page/home-page.component';
+import { MoviesModule } from './movies/movies.module';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MoviesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
